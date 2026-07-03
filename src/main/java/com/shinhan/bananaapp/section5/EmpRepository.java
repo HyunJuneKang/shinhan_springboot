57a5d5b7ca7fb9f93f40095ed07b177786657fa9
HyunJuneKang <halfkang67@naver.com>
2026-07-03 16:13:25 +0900

    ZZ

---
+++ src/main/java/com/shinhan/bananaapp/section5/EmpRepository.java
@@ -0,0 +1,17 @@
+package com.shinhan.bananaapp.section5;
+
+import com.shinhan.bananaapp.dto.AccountDTO;
+import org.springframework.stereotype.Repository;
+
+@Repository("empRepo")
+public class EmpRepository {
+    public AccountDTO getData(){
+        AccountDTO acc = AccountDTO.builder()
+                .accountNo("1234")
+                .ownerName("고대동")
+                .balance(1000L)
+                .accountType("적금")
+                .build();
+        return acc;
+    }
+}
