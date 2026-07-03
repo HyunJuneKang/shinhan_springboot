95b30cd3ec93ecd067a81d7f13a619908eda0054
HyunJuneKang <halfkang67@naver.com>
2026-07-03 16:15:23 +0900

    ZZ

--- src/main/java/com/shinhan/bananaapp/section5/EmpService.java
+++ src/main/java/com/shinhan/bananaapp/section5/EmpService.java
@@ -1,7 +1,10 @@
 package com.shinhan.bananaapp.section5;
 
 import org.springframework.stereotype.Service;
-
+//@Component + 서비스기능
 @Service
 public class EmpService {
+    public void selectService(){
+
+    }
 }
